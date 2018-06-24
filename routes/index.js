641f9108646615396535afc142af9bf58df92f75
OskarHome <oskar.jonczyk.ad@gmail.com>
2018-06-24 23:24:44 +0200

    added login/register views

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { category: req.query.category,categories: ["Laptopy","Laptopy2","Laptopy3"],products: [{name:"Laptop",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop2",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop3",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop2",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop3",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop2",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop3",description:"mozna sie nim wytrzec ale tani"}] });
 });
 
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+
+router.get('/register', function(req, res, next) {
+    res.render('register');
+});
+
 module.exports = router;
