c783068265047cee943bbac94eeeb0410ba39b84
OskarHome <oskar.jonczyk.ad@gmail.com>
2018-06-25 20:45:31 +0200

    added more endpoints/views, fetching from query

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,28 @@ var db = require('../database');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     db.getCategories().then(categories => {
-        db.getProducts().then(products => {
-            res.render('index', {category: req.query.category, categories: categories, products: products})
-        })
+
+        if(req.query.category!==undefined){
+            if(req.query.searchQuery!==undefined){
+                db.getProductsByCategoryAndName(req.query.category,req.query.searchQuery).then(products => {
+                    res.render('index', {category: req.query.category, categories: categories, products: products})
+                });
+            } else {
+                db.getProductsByCategory(req.query.category).then(products => {
+                    res.render('index', {category: req.query.category, categories: categories, products: products})
+                });
+            }
+        } else {
+            if(req.query.searchQuery!==undefined){
+                db.getProductsByName(req.query.searchQuery).then(products => {
+                    res.render('index', {category: req.query.category, categories: categories, products: products})
+                });
+            } else {
+                db.getProducts().then(products => {
+                    res.render('index', {category: req.query.category, categories: categories, products: products})
+                });
+            }
+        }
     });
 
   // res.render('index', { category: req.query.category,categories: ["Laptopy","Laptopy2","Laptopy3"],products: [{name:"Laptop",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop2",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop3",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop2",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop3",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop2",description:"mozna sie nim wytrzec ale tani"},{name:"Laptop3",description:"mozna sie nim wytrzec ale tani"}] });
@@ -34,12 +53,26 @@ router.get('/register', function(req, res, next) {
 
 router.get('/cart', function(req, res, next) {
     //get cart
-    const data = [{name: "lama", price: "1.00"},{name: "kuna", price: "2.00"},{name: "los", price: "13.00"},{name: "jezozwierz", price: "5.00"}];
-    res.render('cart',{items: data});
+    db.getCartProductsForUser(1).then(products => {
+        res.render('cart',{items: products});
+    });
+    ;
+
+
 });
 
 router.post('/checkout', function (req,res,next) {
+    console.log(req.body.products);
+    console.log(req.body.quantities);
    res.render('checkout', {orderID: "1232141421"});
+});
+
+
+router.post('/remove', function (req,res,next) {
+    console.log(req.body.productID);
+
+    //remove from users cart
+    res.redirect("/cart")
 });
 
 module.exports = router;
